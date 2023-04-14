1d7cc9247059729d9fa982c37e219a439314c0aa
Philip Kuo <pkgamma@gmail.com>
2023-04-13 22:40:41 -0500

    followed spotify's "Display your Spotify profile data in a web app" tutorial and made this, added some custom feature to retain already fetched profile

---
+++ src/script.ts
@@ -0,0 +1,110 @@
+const clientId = "dcea37d6562f44058d7e60105f600a53"; // Replace with your client id
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+console.log("Hello world!");
+
+if (localStorage.getItem("profile")) {
+  // if profile has been fetched before, just populate the UI
+  populateUI(JSON.parse(localStorage.getItem("profile")!));
+} else if (!code) {
+  // if no profile has been fetched and no code in url, redirect to auth code flow
+  localStorage.clear();
+  redirectToAuthCodeFlow(clientId);
+} else {
+  // if there's code in url (callback from auth code flow), fetch access token and profile
+  const accessToken = await getAccessToken(clientId, code);
+  console.log("accessToken: ", accessToken);
+  const profile = await fetchProfile(accessToken);
+  console.log("profile: ", profile);
+  localStorage.setItem("profile", JSON.stringify(profile));
+  populateUI(profile);
+}
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("scope", "user-read-private user-read-email");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length: number) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+export async function getAccessToken(
+  clientId: string,
+  code: string
+): Promise<string> {
+  const verifier = localStorage.getItem("verifier");
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("code_verifier", verifier!);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params,
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
+}
+
+async function fetchProfile(token: string): Promise<any> {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return await result.json();
+}
+
+function populateUI(profile: any) {
+  document.getElementById("displayName")!.innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[0].url;
+    document.getElementById("avatar")!.appendChild(profileImage);
+  }
+  document.getElementById("id")!.innerText = profile.id;
+  document.getElementById("email")!.innerText = profile.email;
+  document.getElementById("uri")!.innerText = profile.uri;
+  document
+    .getElementById("uri")!
+    .setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url")!.innerText = profile.href;
+  document.getElementById("url")!.setAttribute("href", profile.href);
+  document.getElementById("imgUrl")!.innerText =
+    profile.images[0]?.url ?? "(no profile image)";
+}
